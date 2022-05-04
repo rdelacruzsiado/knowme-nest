3aaf7d5a803379925b8e9645cae8016574b46f6e
Robinson De La Cruz <rdelacruzsiado@gmail.com>
2022-05-04 12:27:29 -0500

    feat: upload environment variables. feat: create the connection with Postgres

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('config', () => ({
+  postgres: {
+    host: process.env.TYPEORM_HOST,
+    port: parseInt(process.env.TYPEORM_PORT, 10),
+    name: process.env.TYPEORM_DATABASE,
+    user: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+  },
+}));
---
+++ src/database/database.module.ts
@@ -0,0 +1,29 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import config from '../config';
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, name, password, port } = configService.postgres;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: name,
+          synchronize: false,
+          autoLoadEntities: true,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
